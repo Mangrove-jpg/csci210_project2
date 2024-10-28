af81dcd49ce226a06138ab876dff1eb540c58b1f
Stephen Carmon <stephen_carmon@mines.edu>
2024-10-28 13:57:43 -0600

    Rewritten splitpath

--- mkdir_splitpath.c
+++ mkdir_splitpath.c
@@ -33,62 +33,91 @@ void attatchNodeToParent(struct NODE* parent, const char* name, char type) {
 
 //handles tokenizing and absolute/relative pathing options
 struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
-
-    // Create a new node that will track the current directory
+    // Find Whether path is absolute or relative
+    char path_type = pathName[0];
     struct NODE* directory;
-    directory = root;
+    if (path_type == '/')
+    {
+        strcat(dirName, "/");
+        directory = root;
+    }
+    else
+    {
+        directory = cwd;
+    }
 
-    // find first token in pathName
+    // Check if path is only "/"
+    if (strcmp(pathName, "/") == 0)
+    {
+        return directory;
+    }
+
+    // Initialize array of C strings
+    char path_strings[100][64] = {""};
+
+    // Find initial token
     char* token = strtok(pathName, "/");
-    char* lastToken = NULL;
+    int current_token_index = 0;
 
-    // Loop over the rest of the tokens
-    while (token != NULL){
-
-        // Store token in a variable
-        lastToken = token;
+    // Place all tokens into array
+    while(token != NULL && current_token_index < 100)
+    {
+        strcpy(path_strings[current_token_index], token);
         token = strtok(NULL, "/");
-        // Append token to dirname if it isn't last
+        current_token_index += 1;
+    }
 
-        //Check that the Token exists in directory
-        if (token != NULL)
-        {
-            //Check for if token is a child directory
-            struct NODE* child_directory;
-            if (directory->childPtr != NULL)
+    // Check all directories exist on path
+    for (int i = 0; i < current_token_index - 1; i++)
+    {
+
+        // Check if current directory has child directories
+        struct NODE* child_directory;
+        if (directory->childPtr != NULL)
             {
                 child_directory = directory->childPtr;
             }
             else
             {
-                child_directory = directory;
+                printf("ERROR: directory %s does not exist\n", path_strings[i]);
+                return NULL;
             }
-            while(strcmp(lastToken, child_directory->name) != 0)
+
+        // Check if any of the siblings have the same name as the current token
+        while(child_directory != NULL)
+        {
+            // If child exists, set it to directory and break
+            if (strcmp(path_strings[i], child_directory->name) == 0)
             {
-                //If not, send an error
-                if (child_directory->siblingPtr == NULL)
-                {
-                    printf("ERROR: directory %s does not exist\n", lastToken);
-                    return NULL;
-                }
+                directory = child_directory;
+                break;
+            }
+            else
+            {
                 child_directory = child_directory->siblingPtr;
             }
-
-            // Append token to Directory name
-            strcat(dirName, "/");
-            strcat(dirName, lastToken);
-
-            //Update Directory to correct child
-            directory = child_directory;
         }
-        else
+        // If child doesn't exist, send directory doesn't exist error
+        if (child_directory == NULL)
         {
-            strcat(baseName, lastToken); 
+            printf("ERROR: directory %s does not exist\n", path_strings[i]);
+            return NULL;
+            break;
         }
 
-        // Make token basename if it is the final token
     }
-    // Return Directory that basename is in
+
+    // Arrange dirname and basename
+    for (int i = 0; i < current_token_index -1; i++)
+    {
+        strcat(dirName, path_strings[i]);
+        // Don't add / if it's last one
+        if (i != current_token_index - 2)
+        {
+            strcat(dirName, "/");
+        }
+    }
+    strcpy(baseName, path_strings[current_token_index-1]);
     return directory;
 }
 
@@ -98,9 +127,9 @@ void mkdir(char pathName[]){
     // TO BE IMPLEMENTED
     char basename [64];
     char dirname [64];
+    char pathStore[64];
     basename[0] = '\0';
     dirname[0] = '\0';
-    char pathStore[64];
     pathStore[0] = '\0';
     strcpy(pathStore, pathName);
 
@@ -134,8 +163,7 @@ void mkdir(char pathName[]){
         return;
     }
 
-    // allocate space for a new  node
-    
+    // add node to parent directory
     attatchNodeToParent(directory, basename, 'D');
     printf("MKDIR SUCCESS: node %s successfully created\n", pathStore);
 
